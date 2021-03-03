7f4c14e24ae41044dc87eb6921a64c90f6971e1e
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-03-04 00:10:09 +0100

    Added database connection

--- inc/classes/DB.php
+++ inc/classes/DB.php
@@ -4,19 +4,41 @@
     if(!defined('__CONFIG__')){
         die('The configuration file could not be loaded');
     }
+    
+    
 
     // Creating the class DB
     class DB{
         
         protected static $con;
-
+        
         // This function will create a connection with the database
         private function __construct(){
 
-            // Testing if database exists, and if it does, connect to it
+            // Testing if database exists
             try{
-                self::$con = new PDO()
+                include_once 'databasedata.php';
+                self::$con = new PDO('mysql:charset=utf8mb4;host='.$hostName.';port='.$portNumber.';dbname='.$databaseName, $userName, $userPassword);
+                self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+                self::$con->setAttribute( PDO::ATTR_PERSISTENT, false);
+            }
+            catch (PDOException $e){
+                echo('Could not connect to the database');
+                exit;
             }
         }
+
+        // Creating database connection
+        public static function getConnection(){
+
+            // if instace has not been started, start it
+            if(!self::$con){
+                new DB();
+            }
+
+            // Return the writeable database connection
+            return self::$con;
+        }
+
     }
 ?>
