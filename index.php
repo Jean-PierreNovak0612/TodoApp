b3a73e10798010521bba0ae893b7241621eb4304
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-03-04 21:36:02 +0100

    Added bootstrap styling, project is done

--- index.php
+++ index.php
@@ -16,22 +16,22 @@
     ?>
     <title>Selection menu</title>
 </head>
-<body>
+<body class="w-75 mx-auto mt-5">
     <!-- Creating the GUI for adding project -->
     <form class="addproject">
-        <label for="projectName"><h3>Enter the name of the project</h3></label>
-        <input type="text" id="projectName" placeholder="MyProject" required="required">
-        <button type="submit">Enter project</button>
-        <div class="success" style="display:none;"></div>
-        <div class="error" style="display:none"></div>
+        <label for="projectName" class="mb-5"><h3>Enter the name of the project</h3></label>
+        <input type="text" id="projectName" placeholder="MyProject" required="required" class="form-control form-control-lg mb-5">
+        <button type="submit" class="btn btn-dark py-3 px-5">Enter project</button>
+        <div class="success alert alert-success mt-5" style="display:none;"></div>
+        <div class="error alert alert-danger mt-5" style="display:none"></div>
     </form>
 
     <!-- This table will display all currentaly existing projects in the database -->
-    <table>
+    <table class="w-100 mt-5">
         <tr>
-            <th>Project priority</th>
-            <th>Project name</th>
-            <th>Project completion</th>
+            <th class="w-25 py-3 px-5">Project priority</th>
+            <th class="w-25 py-3 px-5">Project name</th>
+            <th class="w-50 py-3 px-5">Project completion</th>
         </tr>
         <?php
             // Getting all project names and their completion levels from database
@@ -43,12 +43,12 @@
             while($data = $getProjects->fetch()) :
         ?>
         <tr>
-            <td><?php echo $iteration; $iteration++; ?></td>
-            <td><?php $projectName = $data['name']; echo $projectName; ?></td>
-            <td><?php if($data['done'] == 0) : ?>
-            Project is not complete yet! <button class="continue" onclick="getName('<?php echo $projectName ?>')">Complete Project</button>
+            <td class="w-25 py-3 px-5"><?php echo $iteration; $iteration++; ?></td>
+            <td class="w-25 py-3 px-5"><?php $projectName = $data['name']; echo $projectName; ?></td>
+            <td class="w-50 py-3 px-5"><?php if($data['done'] == 0) : ?>
+            Project is not complete yet! <button class="continue btn btn-danger ms-3" onclick="getName('<?php echo $projectName ?>')">Complete Project</button>
             <?php else : ?>
-            Project has been completed! <button class="continue" onclick="getName('<?php echo $projectName ?>')">See tasks</button>
+            Project has been completed! <button class="continue btn btn-success ms-3" onclick="getName('<?php echo $projectName ?>')">See tasks</button>
             <?php endif ?></td>
         </tr>
         <?php endwhile ?>
